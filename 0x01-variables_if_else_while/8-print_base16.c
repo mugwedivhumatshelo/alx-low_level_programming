54d2a7675b72f849f9f82903d349278321e7b28d
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2023-06-17 05:14:26 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Prints numbers between 0 to 9 and letters between a to f.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 38; i < 48; i++)
+{
+putchar(i);
+}
+for (i = 87; i < 93; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
