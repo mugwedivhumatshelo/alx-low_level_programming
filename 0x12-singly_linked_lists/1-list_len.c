9c0ea2b3a355a1efadb091318c31bcb322c20503
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2023-07-28 03:37:11 -0700

    This is my commit for 1. List length

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Writes the number of elements in a linked list.
+ * @h: pointer to the list_t list.
+ *
+ * Return: number of elements in h.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t f = 0;
+
+	while (h)
+	{
+		f++;
+		h = h->next;
+	}
+	return (f);
+}
