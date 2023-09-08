3c3b2ccbc34f06fcc080f56c0b389f0ba0f0f40f
mugwedivhumatshelo <mugwedivhumatshelo@gmail.com>
2023-09-08 12:36:05 -0700

    create file commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,12 +3,14 @@
  * create_file -Create a file.
  * @filename A pointer to a name of a file to create.
  * @text_content:A pointer to the string to write to a file.
- * Return:1 on success, -1 on failure.
+ *
+ * Return:1 on success,
+ * -1 on failure.
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd,v, len = 0;
+	int fd, v, len = 0;
 
 	if (filename == NULL)
 		return (-1);
